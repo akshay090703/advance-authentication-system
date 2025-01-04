6d6aa533824275db63c96b9b60f4a736aec24a58
akshay090703 <3551akshay@gmail.com>
2025-01-04 18:15:44 +0530

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,16 @@
+import { getEnv } from "../lib/utils/get-env";
+
+const appConfig = () => ({
+  PORT: getEnv("PORT", "5000"),
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  APP_ORIGIN: getEnv("APP_ORIGIN", "http://localhost:5000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+  JWT: {
+    SECRET: getEnv("JWT_SECRET"),
+    EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "1h"),
+    REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET"),
+    REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN", "30d"),
+  },
+});
+
+export const config = appConfig();
